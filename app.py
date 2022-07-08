5d3d5550fcdf40a6754d51ead9ef33921ccddfd0
iuyuuu <108920364+iuyuuu@users.noreply.github.com>
2022-07-08 19:27:02 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
 line_bot_api = LineBotApi(wwqgHPdvCSdYslvVmA/m9CLnmpIw/KZWI6UyPYyx8R9GryRTVPVV77tfot2oKvdLl/ZERWdk21UNzUXAYvVx1JlKU8nfZYjw/e280Gw8KyjpVzym4/CkhZ/2hnNQ/dNXnL2wLAbaTzbIFn6AbAdjsQdB04t89/1O/w1cDnyilFU=)
-
 # 必須放上自己的Channel Secret
 handler = WebhookHandler(661c4caeccec933cb9687d4a917df32f)
 #歡迎詞
@@ -22,19 +21,19 @@ e_bot_api.push_message(U3aa09e9c07cb88c8b2a790f69dbea42d, TextSendMessage(text='
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
-  # get X-Line-Signature header value
-  signature = request.headers['X-Line-Signature']
-  # get request body as text
-   body = request.get_data(as_text=True)
-   app.logger.info("Request body: " + body)
+# get X-Line-Signature header value
+signature = request.headers['X-Line-Signature']
+# get request body as text
+body = request.get_data(as_text=True)
+app.logger.info("Request body: " + body)
 
-  # handle webhook body
-  try:
-    handler.handle(body, signature)
-  except InvalidSignatureError:
-    abort(400)
+# handle webhook body
+try:
+  handler.handle(body, signature)
+except InvalidSignatureError:
+  abort(400)
 
-  return 'OK'
+return 'OK'
 
 #訊息傳遞區塊
 ##### 基本上程式編輯都在這個function #####
@@ -45,5 +44,5 @@ def handle_message(event):
 
 #主程式
 import os if __name__ == "__main__":
-   port = int(os.environ.get('PORT', 5000))
-   app.run(host='0.0.0.0', port=port)
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
