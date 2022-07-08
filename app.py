fe127269e02a3fddf438d5a4fdf912b34a68347e
iuyuuu <108920364+iuyuuu@users.noreply.github.com>
2022-07-09 00:04:50 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ line_bot_api = LineBotApi('wwqgHPdvCSdYslvVmA/m9CLnmpIw/KZWI6UyPYyx8R9GryRTVPVV7
 # 必須放上自己的Channel Secret
 handler = WebhookHandler('661c4caeccec933cb9687d4a917df32f')
 #歡迎詞
-e_bot_api.push_message('U3aa09e9c07cb88c8b2a790f69dbea42d', TextSendMessage(text='Start!'))
+line_bot_api.push_message('U3aa09e9c07cb88c8b2a790f69dbea42d', TextSendMessage(text='Start!'))
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
