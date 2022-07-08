22a5768a37213c6c9f748770e20f7a297ec2bead
iuyuuu <108920364+iuyuuu@users.noreply.github.com>
2022-07-08 23:12:45 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ app = Flask(__name__)
 
 
 # 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi(wwqgHPdvCSdYslvVmA/m9CLnmpIw/KZWI6UyPYyx8R9GryRTVPVV77tfot2oKvdLl/ZERWdk21UNzUXAYvVx1JlKU8nfZYjw/e280Gw8KyjpVzym4/CkhZ/2hnNQ/dNXnL2wLAbaTzbIFn6AbAdjsQdB04t89/1O/w1cDnyilFU=)
+line_bot_api = LineBotApi('wwqgHPdvCSdYslvVmA/m9CLnmpIw/KZWI6UyPYyx8R9GryRTVPVV77tfot2oKvdLl/ZERWdk21UNzUXAYvVx1JlKU8nfZYjw/e280Gw8KyjpVzym4/CkhZ/2hnNQ/dNXnL2wLAbaTzbIFn6AbAdjsQdB04t89/1O/w1cDnyilFU=')
 # 必須放上自己的Channel Secret
-handler = WebhookHandler(661c4caeccec933cb9687d4a917df32f)
+handler = WebhookHandler('661c4caeccec933cb9687d4a917df32f')
 #歡迎詞
-e_bot_api.push_message(U3aa09e9c07cb88c8b2a790f69dbea42d, TextSendMessage(text='Start!'))
+e_bot_api.push_message('U3aa09e9c07cb88c8b2a790f69dbea42d', TextSendMessage(text='Start!'))
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
