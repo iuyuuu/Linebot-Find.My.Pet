d5f794c5479d4df47343e350da5dcf66b13d02e8
iuyuuu <108920364+iuyuuu@users.noreply.github.com>
2022-07-09 17:11:07 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -44,6 +44,11 @@ def callback():
 
 # 處理訊息
 
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)) 
 
 import re
 @handler.add(MessageEvent, message=TextMessage)
