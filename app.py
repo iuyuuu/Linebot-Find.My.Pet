7ebe8828f484d731bb6855052e2f415789854bf9
iuyuuu <108920364+iuyuuu@users.noreply.github.com>
2022-07-11 12:07:37 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def callback():
 
     # handle webhook body
     try:
-        handler.handle(body, signature)
+        handler.handle(body,signature)
     except InvalidSignatureError:
         print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
@@ -48,12 +48,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text)) 
-    if re.match("Start",message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage("Main Functions:","\n","Enter the city you live in to find the closest animal shelter. EX:台北市"))
-    else:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(message))
-
+        TextSendMessage(text=event.message.text))
 
 if __name__ == "__main__":
     app.run()
