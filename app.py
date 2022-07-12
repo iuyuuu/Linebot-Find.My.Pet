c48588f0f581f91253f86d323a8e34465211c23f
iuyuuu <108920364+iuyuuu@users.noreply.github.com>
2022-07-12 21:15:10 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -56,87 +56,51 @@ def handle_message(event):
         message = TextSendMessage(text=('Main Functions: 1. Enter the city you live in to find the closest animal shelter. EX:台北市 2. Enter "Adopt" to get more information'))
         line_bot_api.reply_message(event.reply_token, message)
     elif '台北市' in message:  #輸入城市可得附近得流浪動物收容所
-        line_bot_api.reply_message(event.reply_token,LocationSendMessage(title='臺北市動物之家', address='Taipei', latitude=25.063149585995905, longitude=121.60929481112795))
-    #輸入城市可得附近得流浪動物收容所 
+        line_bot_api.reply_message(event.reply_token,LocationSendMessage(title='臺北市動物之家', address='臺北市', latitude=25.063149585995905, longitude=121.60929481112795))
     elif '新北市' in message:
-        message = TextSendMessage(text=('新北市政府動物保護防疫處'))
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token,LocationSendMessage(title='新北市政府動物保護防疫處', address='新北市', latitude=25.00416243204753, longitude=121.46037156201673))
     elif '台中市' in message:
-        message = TextSendMessage(text=('臺中市動物之家南屯園區, 臺中市動物之家后里園區'))
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token,LocationSendMessage(title='臺中市動物之家南屯園區', address='臺中市', latitude=24.147963134061612, longitude=120.57528232641883))
     elif '台南市' in message:
-        message = TextSendMessage(text=('臺南市動物之家灣裡站, 臺南市動物之家善化站'))
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token,LocationSendMessage(title='臺南市動物之家灣裡站', address='臺南市', latitude=22.936919635099983, longitude=120.1944557129039))
     elif '高雄市' in message:
-        message = TextSendMessage(text=('高雄市壽山動物保護教育園區, 高雄市燕巢動物保護關愛園區'))
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token,LocationSendMessage(title='高雄市壽山動物保護教育園區', address='高雄市', latitude=22.63716562887085, longitude=120.27868294302546))
     elif '桃園市' in message:
-        message = TextSendMessage(text=('桃園市動物保護教育園區'))
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token,LocationSendMessage(title='桃園市動物保護教育園區', address='桃園市', latitude=25.00869946587133, longitude=121.02776236876359))
     elif '宜蘭縣' in message:
-        message = TextSendMessage(text=('宜蘭縣流浪動物中途之家'))
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token,LocationSendMessage(title='宜蘭縣流浪動物中途之家', address='宜蘭縣', latitude=24.66734722948268, longitude=121.83122645526362))
     elif '新竹縣' in message:
-        message = TextSendMessage(text=('新竹縣公立動物收容所'))
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token,LocationSendMessage(title='新竹縣公立動物收容所', address='新竹縣', latitude=24.82864328995426, longitude=121.01505416876023))
     elif '苗栗縣' in message:
-        message = TextSendMessage(text=('苗栗縣生態保育教育中心'))
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token,LocationSendMessage(title='苗栗縣生態保育教育中心', address='苗栗縣', latitude=24.499868218200465, longitude=120.7940121975897))
     elif '彰化縣' in message:
-        message = TextSendMessage(text=('彰化縣流浪狗中途之家'))
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token,LocationSendMessage(title='彰化縣流浪狗中途之家', address='彰化縣', latitude=23.969424242283026, longitude=120.6197269264156))
     elif '南投縣' in message:
-        message = TextSendMessage(text=('南投縣公立動物收容所'))
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '雲林縣' in message:
-        message = TextSendMessage(text=('雲林縣流浪動物收容所'))
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token,LocationSendMessage(title='南投縣公立動物收容所', address='南投縣', latitude=23.90609246989696, longitude=120.66984445524996))
     elif '嘉義縣' in message:
-        message = TextSendMessage(text=('嘉義縣流浪犬中途之家'))
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '屏東縣' in message:
-        message = TextSendMessage(text=('屏東縣公立犬貓中途之家	'))
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token,LocationSendMessage(title='嘉義縣流浪犬中途之家', address='嘉義縣', latitude=23.593276999927433, longitude=120.49980383848104))
     elif '臺東縣' in message:
-        message = TextSendMessage(text=('臺東縣動物收容中心'))
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token,LocationSendMessage(title='臺東縣動物收容中心', address='臺東縣', latitude=22.721962801874383, longitude=121.10053214457591))
     elif '花蓮縣' in message:
-        message = TextSendMessage(text=('花蓮縣狗貓躍動園區'))
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token,LocationSendMessage(title='花蓮縣狗貓躍動園區', address='花蓮縣', latitude=23.805844864623193, longitude=121.4981204264126))
     elif '澎湖縣' in message:
-        message = TextSendMessage(text=('澎湖縣流浪動物收容中心'))
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '基隆市' in message:
-        message = TextSendMessage(text=('基隆市寵物銀行'))
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '新竹市' in message:
-        message = TextSendMessage(text=('新竹市動物保護教育園區'))
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '嘉義市' in message:
-        message = TextSendMessage(text=('嘉義市動物保護教育園區'))
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '金門縣' in message:
-        message = TextSendMessage(text=('金門縣動物收容中心'))
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '連江縣' in message:
-        message = TextSendMessage(text=('連江縣流浪犬收容中心'))
-        line_bot_api.reply_message(event.reply_token, message)
-    elif "Adopt" in message:
+        line_bot_api.reply_message(event.reply_token,LocationSendMessage(title='澎湖縣流浪動物收容中心', address='澎湖縣', latitude=23.55242754587402, longitude=119.62716313990182))
+    elif "Adopt" in message:  #Carousel linebot選單可以連接到指定網站
         buttons_template_message = TemplateSendMessage(
-        alt_text = "股票資訊",
+        alt_text = "Adopt Information",
         template=CarouselTemplate(
             columns=[
                 CarouselColumn(
-                thumbnail_image_url ="https://images.unsplash.com/photo-1574227114640-234776e0d217?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1016&q=80",
-                title = message + " 股票資訊",
-                text ="請點選想查詢的股票資訊",
+                thumbnail_image_url ="https://images.unsplash.com/photo-1570824105192-a7bb72b73141?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1442&q=80",
+                title = message + " Information",
+                text ="Please click the information you need",
                 actions =[
                     MessageAction(
-                        label= message[3:] + " 個股資訊",
-                        text= "個股資訊 " + message[3:]),
+                        label= "Stray Animals Matching Platform",
+                        uri= "https://asms.coa.gov.tw/Amlapp/App/AnnounceMent.aspx?PageType=Transfer"),
                     MessageAction(
-                        label= message[3:] + " 個股新聞",
-                        text= "個股新聞 " + message[3:])
+                        label= "Shelters in Taiwan",
+                        uri= "https://asms.coa.gov.tw/Amlapp/App/PetsMap1.aspx")
                         ]
                     )
                 ]
@@ -144,11 +108,8 @@ def handle_message(event):
         )
         line_bot_api.reply_message(event.reply_token, buttons_template_message)
     else:
-        message = TextSendMessage(text=('Please enter "Start"'))
+        message = TextSendMessage(text=('Please enter "Start"')) #輸入無法判斷的訊息會跳回請輸入Start
         line_bot_api.reply_message(event.reply_token, message)
-
-
-
 
 
 
